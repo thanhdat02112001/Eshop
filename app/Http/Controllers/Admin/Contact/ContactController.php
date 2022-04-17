a691b84b675e0147da6f75e6eb838c1c3b341db8
subia2 <11194249@st.neu.edu.vn>
2022-04-17 23:58:07 +0700

    Contact

---
+++ app/Http/Controllers/Admin/Contact/ContactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Contact;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index () {
+        $contacts = Contact::paginate(10);
+
+        return view('backend.contact.contact', compact('contacts'));
+    }
+
+    public function delete ($id) {
+        Contact::destroy($id);
+
+    }
+}
