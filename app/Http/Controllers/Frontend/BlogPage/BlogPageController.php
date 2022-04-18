eeacb4a2bbcb657a78ffac5a3769c896caef52a3
DuyMai2001 <102887102+DuyMai2001@users.noreply.github.com>
2022-04-18 07:26:28 +0700

    Mai

--- app/Http/Controllers/Frontend/BlogPage/BlogPageController.php
+++ app/Http/Controllers/Frontend/BlogPage/BlogPageController.php
@@ -1,1 +1,24 @@
-abc
+<?php
+
+namespace App\Http\Controllers\Frontend\BlogPage;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogPageController extends Controller
+{
+    public function index () {
+        $newBlogs = Blog::orderBy('created_at', 'DESC')->paginate(4);
+        $listBlogs = Blog::paginate(4);
+
+        return view ('frontend.blog', compact('newBlogs', 'listBlogs'));
+    }
+
+    public function detail ($id) {
+        $newBlogs = Blog::orderBy('created_at', 'DESC')->paginate(4);
+        $blog = Blog::find($id);
+
+        return view('frontend.detailblog', compact('newBlogs', 'blog'));
+    }
+}
